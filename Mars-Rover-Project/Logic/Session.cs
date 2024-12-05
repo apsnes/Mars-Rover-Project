ddadb3eb5865201bdaba5005172e3fa5ea194f43
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-12-05 17:05:02 +0000

    Edit layout of WPF

--- Mars-Rover-Project/Logic/Session.cs
+++ Mars-Rover-Project/Logic/Session.cs
@@ -11,26 +11,26 @@ namespace Mars_Rover_Project.Logic
     public class Session : BaseClass
     {
         private List<Rover> rovers = new();
-        private Rover currentRover = new();
+        public Rover CurrentRover = new();
         public void AddRover()
         {
             rovers.Add(new Rover());
         }
         public void Move()
         {
-            currentRover.Move();
+            CurrentRover.Move();
         }
         public void TurnLeft()
         {
-            currentRover.Instruct("L");
+            CurrentRover.Instruct("L");
         }
         public void TurnRight()
         {
-            currentRover.Instruct("R");
+            CurrentRover.Instruct("R");
         }
         public void SetCurrentRover(Rover rover)
         {
-            currentRover = rover;
+            CurrentRover = rover;
         }
     }
 }
--- WPF_Mars_Rover/MainWindow.xaml.cs
+++ WPF_Mars_Rover/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,7 @@ namespace WPF_Mars_Rover
         {
             InitializeComponent();
             DataContext = _session;
+
             PlateauSize instance = PlateauSize.GetInstance();
             int cols = instance.Width;
             int rows = instance.Height;
@@ -51,6 +53,17 @@ namespace WPF_Mars_Rover
                     MatrixGrid.Children.Add(rectangle);
                 }
             }
+
+            Rover currentRover = _session.CurrentRover;
+            Rectangle roverAsRectangle = new Rectangle
+            {
+                Fill = new SolidColorBrush(Colors.DarkBlue),
+                Stroke = new SolidColorBrush(Colors.White),
+                StrokeThickness = 2
+            };
+            Grid.SetRow(roverAsRectangle, currentRover.Position.X);
+            Grid.SetColumn(roverAsRectangle, currentRover.Position.Y);
+            MatrixGrid.Children.Add(roverAsRectangle);
         }
         private void OnClickMove(object sender, RoutedEventArgs e)
         {
