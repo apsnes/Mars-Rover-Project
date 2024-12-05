ce22628a76f857c17692e08dfeff603a4277dc51
joel.wright.cs@gmail.com <joel.wright.cs@gmail.com>
2024-12-05 10:02:00 +0000

    Create project files

---
+++ Mars-Rover-Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mars_Rover_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
